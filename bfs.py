edbd6cb4e0c9c7b16f5fa210538beb677d213779
Jesse Schneider <jessewschneider96@gmail.com>
2020-03-10 21:10:28 +1000

    generate board func added, print board added, and completed basic BFS searching algorithm

--- bfs.py
+++ bfs.py
@@ -1,40 +1,61 @@
 from queue import Queue
 import copy
 
-#values for moving left, right, down, up and right
-# x-axis = [-1, 1, 0, 0 ]
-# y-axis = [0, 0, -1, 1]
+def create_new_n_board(n):
+    queens = [[0 for i in range(n)] for j in range(n)]
+    begin = {
+        "queens": queens,
+        "row": 0,
+        "solutions": 0,
+    }
+    return begin
+
+def print_board(board):
+    for i in range(len(board)):
+            print(board[i])
+    print()
 
 queen = 1
-n = 0
+num = 3
 
 queue = Queue()
 
+begin = create_new_n_board(num)
 
-begin = {
-    "queens" : [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-    ],
+explored = {
+    str(begin["queens"]): True
 }
-
 
 queue.put(begin)
 
-while queue.not_empty():
+while not queue.empty():
     #get the next state in the queue
     current_state = queue.get()
     current_queens = current_state["queens"]
-
-
-    for i in range(3):
+    current_row = current_state["row"]
+    print_board(current_queens)
+    
+    for i in range(num):
+        if current_row >= num:
+            continue
+        #copy the current state, into a new board
         new_queens = copy.deepcopy(current_queens)
-        new_queens[i][n] = 1
 
+        #place the new queen on the new board
+        new_queens[current_row][i] = queen
+        
+        #if this board has already been explored, then do not add back into queue
+        if str(new_queens) in explored:
+            continue
 
+        #create a new state to go in the queue, add 1 to the row to increment for next time
         new_state = {
             "queens": new_queens,
+            "row": current_row + 1,
+            "solutions": 0,
         }
 
-        q.put(new_state)
+        #add this new board into the queue
+        queue.put(new_state)
+        #record the current board in the 'explored' dict - make sure not to explore it again
+        explored[str(new_queens)] = True
